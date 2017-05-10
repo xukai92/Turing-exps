c305437d8c7ac3ae05cef8ded557f0405cb14323
Kai Xu <xukai921110@gmail.com>
2017-05-10 23:44:35 +0100

    Give upgit add .

--- amazon-talk/LDA.infer.jl
+++ amazon-talk/LDA.infer.jl
@@ -1,11 +1,11 @@
 using Distributions, Turing   # load packages
-include("topic.data3.jl")      # load toy dataset
+include("topic.data.jl")      # load toy dataset
 
 # Define the LDA model with parameters:
 #   K   - topic num         w   - word instances
 #   V   - vocabulary      doc   - doc instances
-#   M   - doc num           β   - topic prior
-#   N   - count of words    α   - word prior
+#   M   - doc num           β   - word prior
+#   N   - count of words    α   - topic prior
 @model LDA(K, V, M, N, w, doc, β, α) = begin
   θ = Vector{Vector{Real}}(M)
   for m = 1:M
@@ -17,13 +17,14 @@ include("topic.data3.jl")      # load toy dataset
     ϕ[k] ~ Dirichlet(β)
   end
 
-  z = tzeros(Int, N)    # Turing-safe array
-  for n = 1:N
-    z[n] ~ Categorical(θ[doc[n]])   # z here is unkown
-  end
+  # z = tzeros(Int, N)    # Turing-safe array
+  # for n = 1:N
+  #   z[n] ~ Categorical(θ[doc[n]])   # z here is unkown
+  # end
 
   for n = 1:N
-    w[n] ~ Categorical(ϕ[z[n]])
+    phi_dot_theta = [dot(map(p -> p[i], ϕ), θ[doc[n]]) for i = 1:V]
+    w[n] ~ Categorical(phi_dot_theta)
   end
 end
 
@@ -34,7 +35,8 @@ end
 #   * adaptation-step-num 200, target-accept-rate 0.65 and length 1.5 for HMCDA; 1 iteration in each Gibbs
 samples = sample(
   LDA(data=topicdata),
-  Gibbs(1000, PG(50, 1, :z), HMCDA(200, 0.65, 1.5, :θ, :ϕ))
+  HMCDA(1000, 0.65, 1.5)
+  # Gibbs(1000, PG(50, 1, :z), HMCDA(200, 0.65, 1.5, :θ, :ϕ))
 )
 
 
@@ -45,6 +47,7 @@ samples = sample(
 
 K = topicdata["K"]
 V = topicdata["V"]
+M = topicdata["M"]
 
 using Gadfly
 using DataFrames
@@ -52,17 +55,17 @@ Gadfly.push_theme(:dark)
 
 makerectbinplot(i, fn) = begin
 
-  ϕarr = mean(samples[:ϕ][1:i])
-  ϕ = [ϕarr[1]'; ϕarr[2]'; ϕarr[3]'; ϕarr[4]']
+  θarr = mean(samples[:θ][1:i])
+  θ = [θarr[1]'; θarr[2]';θarr[3]';θarr[4]';θarr[5]';θarr[6]';θarr[7]';θarr[8]';θarr[9]';θarr[10]';θarr[11]';θarr[12]';θarr[13]';θarr[14]';θarr[15]';θarr[16]';θarr[17]';θarr[18]';θarr[19]';θarr[20]';θarr[21]';θarr[22]';θarr[23]';θarr[24]';θarr[25]']
 
 
-  df = DataFrame(Topic = vec(repmat(collect(1:K)', V, 1)), Word = vec(repmat(collect(1:V)', 1, K)), Probability = vec(ϕ))
+  df = DataFrame( Topic= vec(repmat(collect(1:K)', M, 1)), Doc = vec(repmat(collect(1:M)', 1, K)), Probability = vec(θ))
 
-  p = plot(df,x=:Word, y=:Topic, color=:Probability, Geom.rectbin)
+  p = plot(df,x=:Topic, y=:Doc, color=:Probability, Geom.rectbin)
 
   draw(PNG("$fn$i.png", 6inch, 4.5inch), p)
 end
 
-for i = 1:length(samples[:ϕ])
-  makerectbinplot(i, "frames/LDA")
+for i = 1:length(samples[:θ])
+  makerectbinplot(i, "frames/LDA-theta")
 end
